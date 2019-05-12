3eafab3dc46f57505c719e9cd73ef27f595f313a
saikksub <saikksub@gmail.com>
2019-05-12 22:17:23 +0530

    Created API

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # node-docx-tables
 Node library to extract tables from docx (office open xml) documents
+
+## Install
+``` bash
+npm i --save node-docx-tables
+```
+
+## Usage
+``` JavaScript
+const docxTables = require('docx-tables')
+
+docxTables({
+  file: 'path/to/the/docx/file'
+}).then((data) => {
+  // .docx table data
+  console.log(data)
+}).catch((error) => {
+  console.error(error)
+})
+```
+
+### Example
+
+| | | |
+|-|-|-|
+| a | b |
+| c | d |
+
+A .docx file containing above table will result following JSON output:
+``` JSON
+{ '0':
+   [ { position: { row: 0, col: 0 }, data: 'a\n' },
+     { position: { row: 0, col: 1 }, data: 'b\n' } ],
+  '1':
+   [ { position: { row: 1, col: 0 }, data: 'c\n' },
+     { position: { row: 1, col: 1 }, data: 'd\n' } ] }
+```
+
+# License
+[MIT](https://opensource.org/licenses/MIT)
+
+## Author
+[saikksub](https://github.com/saikksub)
--- index.js
+++ index.js
@@ -1,70 +1,116 @@
 'use strict'
 
 const fs = require('fs')
+const path = require('path')
 const JSZip = require('jszip')
 const convert = require('xml-js')
-const fsExtra = require('fs-extra')
 
-fs.readFile('./test2.docx', function (err, data) {
-  if (err) throw err
-  JSZip.loadAsync(data).then(function (zip) {
-    if (zip.files['word/document.xml']) {
-      zip.files['word/document.xml'].async("binarystring").then(function (content) {
-        let xml = convert.xml2json(content, {compact: true, spaces: 4})
-        const result = {}
-        if (typeof xml === 'string') {
-          xml = JSON.parse(xml)
-        }
-        fsExtra.writeJsonSync('./doc.json', xml, { spaces: 4 })
-        try {
-          let wTable = xml['w:document']['w:body']['w:tbl']
-          if (wTable) {
-            if (wTable.constructor !== [].constructor) {
-              wTable = [ wTable ]
-            }
-            wTable.forEach((wTableItem) => {
-              const wTableItemRow = wTableItem['w:tr']
-              wTableItemRow.forEach((wTableItemRowItem, rowIndex) => {
-                const wTableItemRowColumn = wTableItemRowItem['w:tc']
-                const rowObject = []
-                wTableItemRowColumn.forEach((wTableItemRowColumnItem, colIndex) => {
-                  let wp = wTableItemRowColumnItem['w:p']
-                  if (wp) {
-                    if ( wp.constructor !== [].constructor) {
-                      wp = [ wp ]
-                    }
-                    let data = ''
-                    wp.forEach((wpItem) => {
-                      if (wpItem['w:r'] && wpItem['w:r']['w:t'] && wpItem['w:r']['w:t']._text) {
-                        // console.log(wpItem['w:r']['w:t'])
-                        data += `${wpItem['w:r']['w:t']._text}\n`
-                      }
-                    })
-                    if (data) {
-                      rowObject.push({
-                        position: {
-                          row: rowIndex,
-                          col: colIndex
-                        },
-                        data 
-                      })
-                    }
-                  }
-                  // console.log('++++++++++++++++++')
-                })
-                if (rowObject && rowObject.constructor === [].constructor && rowObject.length > 0) {
-                  result[`${rowIndex}`] = Object.assign([], rowObject)
+/**
+ * Load and Extract given docx file
+ */
+async function loadFile (file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, function (err, data) {
+      if (err) {
+        reject(err)
+      }
+      JSZip.loadAsync(data).then(function (zip) {
+        resolve(zip)
+      }).catch((error) => {
+        reject(error)
+      })
+    })
+  })
+}
+
+/**
+ * Main Logic for extracting Table data from XML JSON data
+ */
+function parseTables (xmlJsonData) {
+  const result = {}
+  try {
+    let wTable = xmlJsonData['w:document']['w:body']['w:tbl']
+    if (wTable) {
+      if (wTable.constructor !== [].constructor) {
+        wTable = [ wTable ]
+      }
+      wTable.forEach((wTableItem) => {
+        const wTableItemRow = wTableItem['w:tr']
+        wTableItemRow.forEach((wTableItemRowItem, rowIndex) => {
+          const wTableItemRowColumn = wTableItemRowItem['w:tc']
+          const rowObject = []
+          wTableItemRowColumn.forEach((wTableItemRowColumnItem, colIndex) => {
+            let wp = wTableItemRowColumnItem['w:p']
+            if (wp) {
+              if ( wp.constructor !== [].constructor) {
+                wp = [ wp ]
+              }
+              let data = ''
+              wp.forEach((wpItem) => {
+                if (wpItem['w:r'] && wpItem['w:r']['w:t'] && wpItem['w:r']['w:t']._text) {
+                  data += `${wpItem['w:r']['w:t']._text}\n`
                 }
-                // console.log('==========================')
-              }) 
-            })
+              })
+              if (data) {
+                rowObject.push({
+                  position: {
+                    row: rowIndex,
+                    col: colIndex
+                  },
+                  data 
+                })
+              }
+            }
+            // console.log('++++++++++++++++++')
+          })
+          if (rowObject && rowObject.constructor === [].constructor && rowObject.length > 0) {
+            result[`${rowIndex}`] = Object.assign([], rowObject)
           }
-        } catch (error) {
-          throw error
-        }
-
-        fsExtra.writeJsonSync('./out.json', result, { spaces: 4 })
+          // console.log('==========================')
+        }) 
       })
     }
+  } catch (error) {
+    return error
+  }
+
+  return result
+}
+
+module.exports = function (props) {
+  return new Promise((resolve, reject) => {
+    if (!(props && props.constructor === {}.constructor)) {
+      reject(new Error(`Invalid Props`))
+    }
+    if (!props.file) {
+      reject(new Error(`Object prop "file" is required.`))
+    }
+    if (!fs.existsSync(props.file)) {
+      reject(new Error(`Input file "${props.file}" does not exists. Please provide valid file.`))
+    }
+    // Load and extract docx file
+    loadFile(props.file).then((data) => {
+      if (data.files['word/document.xml']) {
+        data.files['word/document.xml'].async("binarystring").then(function (content) {
+          // Parse XML data
+          let xmlJsonData = convert.xml2json(content, {compact: true, spaces: 4})
+          // If the XML data is invalid, resolve empty object
+          if (!xmlJsonData) {
+            resolve({})
+          }
+          // Make sure parsed XML file is an object
+          if (typeof xmlJsonData === 'string') {
+            xmlJsonData = JSON.parse(xmlJsonData)
+          }
+          
+          const result = parseTables(xmlJsonData)
+          resolve(result)
+        })
+      } else {
+        resolve({})
+      }
+    }).catch((error) => {
+      reject(error)
+    })
   })
-})
+}
