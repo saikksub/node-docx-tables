3aedbd82eaca7ce43fef76574a71c66ac8388a0b
Sai K K <saikksub@gmail.com>
2024-02-19 12:12:24 +0530

    fixes #7

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict'
 
 const fs = require('fs')
-const path = require('path')
 const JSZip = require('jszip')
-const convert = require('xml-js')
+const xml2js = require('xml2js');
+
 
 /**
  * Load and Extract given docx file
  */
-async function loadFile (file) {
+async function loadFile(file) {
   return new Promise((resolve, reject) => {
     fs.readFile(file, function (err, data) {
       if (err) {
@@ -26,13 +26,13 @@ async function loadFile (file) {
 /**
  * Main Logic for extracting Table data from XML JSON data
  */
-function parseTables (xmlJsonData) {
+function parseTables(xmlJsonData) {
   const tables = []
   try {
     let wTable = xmlJsonData['w:document']['w:body']['w:tbl']
     if (wTable) {
       if (wTable.constructor !== [].constructor) {
-        wTable = [ wTable ]
+        wTable = [wTable]
       }
       wTable.forEach((wTableItem) => {
         const result = {}
@@ -43,33 +43,33 @@ function parseTables (xmlJsonData) {
           wTableItemRowColumn.forEach((wTableItemRowColumnItem, colIndex) => {
             let wp = wTableItemRowColumnItem['w:p']
             if (wp) {
-              if ( wp.constructor !== [].constructor) {
-                wp = [ wp ]
+              if (wp.constructor !== [].constructor) {
+                wp = [wp]
               }
               let data = ''
               wp.forEach((wpItem) => {
-                if (wpItem['w:r'] && wpItem['w:r']['w:t'] && wpItem['w:r']['w:t']._text) {
-                  data += `${wpItem['w:r']['w:t']._text}\n`
+                if (wpItem['w:r'] && wpItem['w:r']['w:t'] && (typeof wpItem['w:r']['w:t'] === 'string' || typeof wpItem['w:r']['w:t']._text === 'string')) {
+                  data += `${Buffer.from(wpItem['w:r']['w:t'] || wpItem['w:r']['w:t']._text, 'binary').toString('utf-8')}\n`
                 }
               })
               //if (data) {
-                rowObject.push({
-                  position: {
-                    row: rowIndex,
-                    col: colIndex
-                  },
-                  data 
-                })
+              rowObject.push({
+                position: {
+                  row: rowIndex,
+                  col: colIndex
+                },
+                data
+              })
               //}
             }
             // console.log('++++++++++++++++++')
           })
           //if (rowObject && rowObject.constructor === [].constructor && rowObject.length > 0) {
-            result[`${rowIndex}`] = Object.assign([], rowObject)
+          result[`${rowIndex}`] = Object.assign([], rowObject)
           //}
           // console.log('==========================')
         })
-        tables.push(result) 
+        tables.push(result)
       })
     }
   } catch (error) {
@@ -77,6 +77,14 @@ function parseTables (xmlJsonData) {
   }
 
   return tables
+}
+
+// Function to remove Byte Order Mark (BOM) if it exists
+function removeBOM(content) {
+  if (content.charCodeAt(0) === 0xFEFF) {
+    return content.slice(1);
+  }
+  return content;
 }
 
 module.exports = function (props) {
@@ -92,21 +100,30 @@ module.exports = function (props) {
     }
     // Load and extract docx file
     loadFile(props.file).then((data) => {
-      if (data.files['word/document.xml']) {
-        data.files['word/document.xml'].async("binarystring").then(function (content) {
-          // Parse XML data
-          let xmlJsonData = convert.xml2json(content, {compact: true, spaces: 4})
-          // If the XML data is invalid, resolve empty object
-          if (!xmlJsonData) {
-            resolve({})
+      const documentKey = Object.keys(data.files).find(key => /word\/document.*\.xml/.test(key))
+
+      if (data.files[documentKey]) {
+        data.files[documentKey].async("binarystring").then(function (content) {
+          if (content.startsWith('ï»¿')) {
+            content = content.substring(3);
           }
-          // Make sure parsed XML file is an object
-          if (typeof xmlJsonData === 'string') {
-            xmlJsonData = JSON.parse(xmlJsonData)
-          }
-          
-          const result = parseTables(xmlJsonData)
-          resolve(result)
+
+          const parser = new xml2js.Parser({ explicitArray: false, ignoreAttrs: false })
+          parser.parseString(content, (err, result) => {
+            if (err) {
+              throw err;
+            }
+
+            let xmlJsonData = JSON.stringify(result, null, 4);
+
+            // Make sure parsed XML file is an object
+            if (typeof xmlJsonData === 'string') {
+              xmlJsonData = JSON.parse(xmlJsonData)
+            }
+
+            const res = parseTables(xmlJsonData)
+            resolve(res)
+          })
         })
       } else {
         resolve({})
